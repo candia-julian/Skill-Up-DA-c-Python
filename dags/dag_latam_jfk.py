e54cb3ce312e6b86cd925f7ef944e883ecdf9286
candia-julian <candiajuliane@gmail.com>
2022-12-01 11:58:19 -0300

    agregado astronomer

--- dags/dag_latam_jfk.py
+++ dags/dag_latam_jfk.py
@@ -4,7 +4,8 @@ import pathlib
 import logging
 from airflow import DAG
 from datetime import timedelta,  datetime
-
+#
+from airflow.operators import task
 #postgre operator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 #python operator para ejectuar scripts de python
@@ -13,9 +14,16 @@ from airflow.operators.python import PythonOperator
 from airflow.providers.amazon.aws.transfers import LocalFilesystemToS3Operator
 
 
+
 with DAG(
-    'tutorial_dags',
-    description='Este es un tutorial de dags',
-    schedule_interval=timedelta(days = 1),
+    'dag universidad latam y jfk',
+    description='ETL',
+    schedule_interval="@hourly",
+    catchup=False,
+    default_args={
+        "retries": 5, # If a task fails, it will retry 2 times.
+    },
+    tags=['example'],
     start_date=datetime(2022,11,29)
 ) as dag:
+
